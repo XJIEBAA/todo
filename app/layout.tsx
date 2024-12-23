dda868ff9b1b653812a46e2854a217de93394423
XJIEBAA <matveyreut722@gmail.com>
2024-12-23 15:18:37 +0300

    sync(project): update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
 import type { ReactNode } from "react";
 import { StoreProvider } from "./StoreProvider";
-import { Nav } from "./components/Nav";
+
+import { Nothing } from "./components/nothing/nothing";
+import { Input } from "./components/input/input";
+
+import Logo from "../public/logo.png";
 
 import "./styles/globals.css";
-import styles from "./styles/layout.module.css";
 
 interface Props {
   readonly children: ReactNode;
@@ -15,69 +17,21 @@ export default function RootLayout({ children }: Props) {
     <StoreProvider>
       <html lang="en">
         <body>
-          <section className={styles.container}>
-            <Nav />
+          <div className="logo">
+            <img src={Logo.src}/>
+          </div>
+          <div className="container">
+            <div className="h1">
+              Today
+            </div>
+            <br />
+            <Input/>
+            <br />
+            <Todos/>
 
-            <header className={styles.header}>
-              <Image
-                src="/logo.svg"
-                className={styles.logo}
-                alt="logo"
-                width={100}
-                height={100}
-              />
-            </header>
-
-            <main className={styles.main}>{children}</main>
-
-            <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://reselect.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Reselect
-              </a>
-            </footer>
-          </section>
+            <br /><br /><br /><br />
+            <Nothing/>
+          </div>
         </body>
       </html>
     </StoreProvider>
