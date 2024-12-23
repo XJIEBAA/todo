aba9bb246656084008d1ba7bd51e1e647558fa95
XJIEBAA <matveyreut722@gmail.com>
2024-12-23 18:15:54 +0300

    sync(project): update nothing

--- app/components/nothing/nothing.tsx
+++ app/components/nothing/nothing.tsx
@@ -4,21 +4,28 @@ import { useState } from "react";
 
 import Image from "../../../public/idle.png"
 
+
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+
+
 // import styles from "./Counter.module.css";
 
 export const Nothing = () => {
+  let todos = useAppSelector((state) => state.todos).todos
+
   return (
     <div>
-      <div className="containerNothing">
-        <img src={String(Image.src)} style={{ width: "80%" }} />
-        <div className="reminder">
-          What else we can do today?
+      {todos.length == 0 ? (
+        <div className="containerNothing">
+          <img src={String(Image.src)} style={{ width: "80%" }} />
+          <div className="reminder">
+            What else we can do today?
+          </div>
+          <div className="pretext">
+          Enjoy the rest of your day and don't forget to share your #TodoistZero awesomeness
+          </div>
         </div>
-        <div className="pretext">
-        Enjoy the rest of your day and don't forget to share your #TodoistZero awesomeness
-        </div>
-      </div>
+      ) : null}
     </div>
   );
 };
