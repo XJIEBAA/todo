33450fba614a7a9441cb21ce7e9ef641a2f7c58c
XJIEBAA <matveyreut722@gmail.com>
2024-12-23 18:15:29 +0300

    sync(project): update input

--- app/components/input/input.tsx
+++ app/components/input/input.tsx
@@ -3,23 +3,37 @@
 import { ChangeEvent, useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { update } from "../../../lib/features/counter/todosSlice";
+
 // import styles from "./Counter.module.css";
 
 export const Input = () => {
   const [data, setData] = useState("")
+  const dispatch = useAppDispatch()
+  let todos = useAppSelector((state) => state.todos)
+
+  // console.log(update)
 
   const change = (e: ChangeEvent<HTMLInputElement>) => {
     setData(e.target.value)
   }
 
+  const recheck = () => {
+    if (data.length > 0) {
+      dispatch(update({ data: data, todos: todos.todos }))
+    } else {
+      return 0
+    }
+  }
+
   return (
     <div className="inputContainer">
         <div>
-            <button className="button">+</button>
+            <button onClick={() => {recheck()}} className="button">+</button>
         </div>
         <div>
             <input className="input" type="text" value={data} onChange={(e) => change(e)}/>
         </div>
-    </div>
+    </div>  
   );
 };
